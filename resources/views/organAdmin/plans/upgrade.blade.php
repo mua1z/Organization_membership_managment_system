e561899e0cbf1af7d730ed59ccdb1a75d9ffb92e
Muaz <muazddu@gmail.com>
2025-09-10 18:17:18 +0300

    4 commit

--- resources/views/organAdmin/plans/upgrade.blade.php
+++ resources/views/organAdmin/plans/upgrade.blade.php
@@ -1,19 +1,17 @@
-@extends('layouts.app')
+<x-app-layout>
+    <div class="container">
+        <h2>Upgrade Your Plan</h2>
 
-@section('content')
-<div class="container">
-    <h2>Upgrade Your Plan</h2>
-
-    <form action="{{ route('organAdmin.plans.upgrade') }}" method="POST">
-        @csrf
-        <select name="plan_id" required>
-            @foreach($plans as $plan)
-                <option value="{{ $plan->id }}" {{ $user->plan_id == $plan->id ? 'selected' : '' }}>
-                    {{ $plan->name }} - ${{ $plan->price }}
-                </option>
-            @endforeach
-        </select>
-        <button type="submit">Upgrade Plan</button>
-    </form>
-</div>
-@endsection
+        <form action="{{ route('organAdmin.plans.upgrade') }}" method="POST">
+            @csrf
+            <select name="plan_id" required>
+                @foreach($plans as $plan)
+                    <option value="{{ $plan->id }}" {{ $user->plan_id == $plan->id ? 'selected' : '' }}>
+                        {{ $plan->name }} - ${{ $plan->price }}
+                    </option>
+                @endforeach
+            </select>
+            <button type="submit">Upgrade Plan</button>
+        </form>
+    </div>
+</x-app-layout>
